7f3934df3ea9427e35f1e006e52727ac627fc904
SammieKei40 <sammiekei9@gmail.com>
2022-05-23 03:29:38 -0700

    Updated

--- _sys/db_connection.php
+++ _sys/db_connection.php
@@ -1,9 +1,19 @@
 <?php
-$db_connection = mysqli_connect("localhost","root","","owlphin");
+
+$cleardb_url = parse_url(getenv("mysql://baefa84ead3c8f:f15bb469@us-cdbr-east-05.cleardb.net/heroku_15391bb2dd5489c?reconnect=true"));
+$cleardb_server = $cleardb_url["us-cdbr-east-05.cleardb.net"];
+$cleardb_username = $cleardb_url["baefa84ead3c8f"];
+$cleardb_password = $cleardb_url["f15bb469"];
+$cleardb_db = substr($cleardb_url["owlphin"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$db_connection = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 if (mysqli_connect_errno()){
-		echo mysqli_connect_error();
-		exit();
+	echo mysqli_connect_error();
+	exit();
 }/* else{
-	echo "Successful connected to database!";
+echo "Successful connected to database!";
 } */
+
 ?>
